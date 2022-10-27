eae40cdef979eec80ee738da333354b1f087dd0d
Jurfest <jurfest.apps@gmail.com>
2022-10-27 15:38:00 -0300

    refactor: change parameters place

--- apps/login/src/app/pages/login/login.component.stories.ts
+++ apps/login/src/app/pages/login/login.component.stories.ts
@@ -19,20 +19,10 @@ export default {
       ],
     }),
   ],
-  // parameters: {
-  //   msw: {
-  //     handlers: [
-  //       rest.post(`${environment.baseUrl}/login`, (req, res, ctx) => {
-  //         return res(
-  //           ctx.json({
-  //             id: '1',
-  //             name: 'Diego',
-  //           })
-  //         );
-  //       }),
-  //     ],
-  //   },
-  // },
+  parameters: {
+    layout: 'fullscreen',
+    panelPosition: 'rigth',
+  },
 } as Meta<LoginComponent>;
 
 const Template: Story<LoginComponent> = (args: LoginComponent) => ({
@@ -41,10 +31,6 @@ const Template: Story<LoginComponent> = (args: LoginComponent) => ({
 
 export const EmptyForm = Template.bind({});
 EmptyForm.args = {};
-EmptyForm.parameters = {
-  layout: 'fullscreen',
-  panelPosition: 'bottom',
-};
 
 // More on interaction testing: https://storybook.js.org/docs/angular/writing-tests/interaction-testing
 export const FilledForm = Template.bind({});
@@ -52,19 +38,21 @@ FilledForm.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
 
   // 👇 Simulate interactions with the component
-  const emailInput = await canvas.getAllByPlaceholderText('Digite seu email')[1];
+  const emailInput = await canvas.getAllByPlaceholderText(
+    'Digite seu email'
+  )[1];
   await userEvent.type(emailInput, 'email@provider.com', {
     delay: 100,
   });
 
-  const passwordInput = await canvas.getAllByPlaceholderText('******')[1]
+  const passwordInput = await canvas.getAllByPlaceholderText('******')[1];
   await userEvent.type(passwordInput, 'a-random-password', {
     delay: 100,
   });
 
   // See https://storybook.js.org/docs/angular/essentials/actions#automatically-matching-args to learn how to setup logging in the Actions panel
   const loginButton = await canvas.getByRole('button');
-  await userEvent.click(loginButton,);
+  await userEvent.click(loginButton);
 
   // NOTE: - NOT WORKING WITH ASYNC PIPE
   // const loggedInMsg = await canvas.getByText('Login realizado!');
