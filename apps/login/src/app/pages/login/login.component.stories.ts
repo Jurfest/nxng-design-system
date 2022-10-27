aef89ff068a3750185fcca95ad2b3006006f9d55
Jurfest <jurfest.apps@gmail.com>
2022-10-27 13:48:00 -0300

    feat: add storybook interactions tests

--- apps/login/src/app/pages/login/login.component.stories.ts
+++ apps/login/src/app/pages/login/login.component.stories.ts
@@ -1,6 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { expect } from '@storybook/jest';
+import { userEvent, within } from '@storybook/testing-library';
+
 import { LoginComponent } from './login.component';
 
 export default {
@@ -41,4 +44,29 @@ EmptyForm.args = {};
 EmptyForm.parameters = {
   layout: 'fullscreen',
   panelPosition: 'bottom',
+};
+
+// More on interaction testing: https://storybook.js.org/docs/angular/writing-tests/interaction-testing
+export const FilledForm = Template.bind({});
+FilledForm.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  // 👇 Simulate interactions with the component
+  const emailInput = await canvas.getAllByPlaceholderText('Digite seu email')[1];
+  await userEvent.type(emailInput, 'email@provider.com', {
+    delay: 100,
+  });
+
+  const passwordInput = await canvas.getAllByPlaceholderText('******')[1]
+  await userEvent.type(passwordInput, 'a-random-password', {
+    delay: 100,
+  });
+
+  // See https://storybook.js.org/docs/angular/essentials/actions#automatically-matching-args to learn how to setup logging in the Actions panel
+  const loginButton = await canvas.getByRole('button');
+  await userEvent.click(loginButton,);
+
+  // NOTE: - NOT WORKING WITH ASYNC PIPE
+  // const loggedInMsg = await canvas.getByText('Login realizado!');
+  // await expect(loggedInMsg).toBeInTheDocument();
 };
