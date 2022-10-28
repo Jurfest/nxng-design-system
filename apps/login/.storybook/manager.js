13a4ed77201cda44b1803524dc494e401a42ca56
Jurfest <jurfest.apps@gmail.com>
2022-10-28 11:22:13 -0300

    chore: update storybook configs

--- apps/login/.storybook/manager.js
+++ apps/login/.storybook/manager.js
@@ -4,5 +4,5 @@ import { themes } from '@storybook/theming';
 addons.setConfig({
   theme: themes.dark,
   panelPosition: 'right',
-  selectedPanel: 'storybook/addon-interactions',
+  selectedPanel: 'storybook/addon-interactions/panel',
 });
